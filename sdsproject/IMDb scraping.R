73f9a5aa92d7f43f09c315deab6abcfe998702a9
Rasmus Gars <Rasmus.gars@gmail.com>
2015-12-04 11:36:12 +0100

    Scraping, cleaning and merging done
    Nu er data blevet cleanet nogenlunde og merged. Man skal selv lige
    loade book.Rda inden koden køres.
    Der er endnu ikke foretaget oversættelse af titlerne, da jeg lige ville
    se hvordan det så ud inden vi gjorde det.

--- sdsproject/IMDb scraping.R
+++ sdsproject/IMDb scraping.R
@@ -1,9 +1,12 @@
 library("rvest")
-library(stringr)
+library("stringr")
 library("dplyr")
 
+#---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------
+
 #Generating links to loop through all movies "based-on-novel".
-for(i in 0:5){
+for(i in 0:3){
   imdb.links=(paste("http://www.imdb.com/search/keyword?keywords=based-on-novel&sort=moviemeter,asc&mode=detail&page=", 0:i, sep = "","&ref_=kw_nxt"))
 }
 head(imdb.links)
@@ -20,7 +23,7 @@ getlinks_imdb = function(link){
 
 #Loop data to a list
 my.imdb.linkdata = list() # initialize empty list
-for (i in imdb.links[1:10]){
+for (i in imdb.links[1:100]){
   print(paste("processing", i, sep = " "))
   my.imdb.linkdata[[i]] = getlinks_imdb(i)
   # waiting one second between hits
@@ -36,8 +39,8 @@ df.imdb = ldply(my.imdb.linkdata)
 imdb.secondary.links=(paste("http://www.imdb.com", sep = "", df.imdb$my.link.link))
 head(imdb.secondary.links)
 
-
-# - - - - - - - - - - - - - - - -- - - - - -- - - - -- - - - - - 
+#---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------
 
 
 #Function to collect links to every movie on the list
@@ -61,21 +64,15 @@ scrape_imdb = function(link2){
   imdb.metascore = my.link2 %>% 
     html_nodes(".star-box-details a:nth-child(4)") %>% 
     html_text()
-  imdb.budget = my.link2 %>% 
-    html_nodes("#titleDetails :nth-child(13)") %>% 
-    html_text()
-  imdb.director = my.link2 %>% 
-    html_nodes("#overview-top :nth-child(8) .itemprop") %>% 
-    html_text()
   imdb.leadactor = my.link2 %>% 
     html_nodes(":nth-child(10) a:nth-child(2) .itemprop") %>% 
     html_text()
-  return(cbind(imdb.title, imdb.rating, imdb.year, imdb.runtime,imdb.numberofratings, imdb.metascore, imdb.budget, imdb.director, imdb.leadactor))
+  return(cbind(imdb.title, imdb.rating, imdb.year, imdb.runtime,imdb.numberofratings, imdb.metascore, imdb.leadactor))
 }
 
 #Loop data to a list
 my.imdb.data = list() # initialize empty list
-for (i in imdb.secondary.links[1:20]){
+for (i in imdb.secondary.links[1:100]){
   print(paste("processing", i, sep = " "))
   my.imdb.data[[i]] = scrape_imdb(i)
   # waiting one second between hits
@@ -85,21 +82,95 @@ for (i in imdb.secondary.links[1:20]){
 
 df.imdb.data = ldply(my.imdb.data)
 
-# END OF SCRAPING - BEGINNING OF CLEANING - - - - - - - - - - - - -
+# END OF SCRAPING
+
+#---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------
+
+# BEGINNING OF CLEANING
+# Cleaning IMDb data
+
+df.imdb.clean=df.imdb.data
 
 #Correcting runtime variable
-df.imdb.data$imdb.runtime = gsub(" min","", df.imdb.data$imdb.runtime) #remove trailing characters
-df.imdb.data$imdb.runtime = gsub("(^ +)|( +$)", "", df.imdb.data$imdb.runtime) #remove trailing blanks
-df.imdb.data$imdb.runtime <- as.numeric(df.imdb.data$imdb.runtime) #Convert to numeric
+df.imdb.clean$imdb.runtime = gsub(" min","", df.imdb.clean$imdb.runtime) #remove trailing characters
+df.imdb.clean$imdb.runtime = gsub("(^ +)|( +$)", "", df.imdb.clean$imdb.runtime) #remove trailing blanks
+df.imdb.clean$imdb.runtime <- as.numeric(df.imdb.clean$imdb.runtime) #Convert to numeric
 
-#Correcting year variable
-df.imdb.data$imdb.year <- as.numeric(df.imdb.data$imdb.year) #Convert to numeric
+#Correcting number of ratins variable:
+df.imdb.clean$imdb.numberofratings = gsub(",","", df.imdb.clean$imdb.numberofratings) #remove thousand separator
+df.imdb.clean$imdb.numberofratings = as.numeric(df.imdb.clean$imdb.numberofratings) #Convert to numeric
 
-#filter on runtime and year, to get rid of TV-shows (Assuming TV-shows are no longer than 65 minutes and relevant movies are longer than 65 minutes + believing that TV-shows have been broadcasted for more than a year - Think about possible improvements for this)
-df.imdb.data = filter(df.imdb.data, df.imdb.data$imdb.runtime > 65 & df.imdb.data$imdb.year > 0)
+#Correcting meta score variable:
+df.imdb.clean$imdb.metascore = gsub("/100","", df.imdb.clean$imdb.metascore) #Change to score out of hundred.
+df.imdb.clean$imdb.metascore = as.numeric(df.imdb.clean$imdb.metascore) #Convert to numeric
+df.imdb.clean$imdb.metascore = df.imdb.clean$imdb.metascore/10 #Divide by 10 to get at number on the same scale as imdb ratings.
+
+
+#filter on runtime, to get rid of TV-shows (Assuming TV-shows are no longer than 65 minutes and relevant movies are longer than 65 minutes - Think about possible improvements for this)
+df.imdb.clean = filter(df.imdb.clean, df.imdb.clean$imdb.runtime > 65)
+
+class(df.imdb.clean$imdb.numberofratings)
+
+#Remove NA's (And thus hopefully removing TV-series)
+df.imdb.clean=na.omit(df.imdb.clean) #THINK ABOUT POSSIBLE PROBLEMS WITH THIS
 
 #Removing irrelevant variables:
-df.imdb.data = subset(df.imdb.data, select = -.id) # .id-string
+df.imdb.clean = subset(df.imdb.clean, select = -.id) # .id-string
+
+#Changing name of the title variable
+df.imdb.clean$title=df.imdb.clean$imdb.title
+
+#Selecting only relevant variables
+df.imdb.clean=select(df.imdb.clean, -imdb.title)
+
+#---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------
+
+#CLEANING GOODREAD DATA
+df.book.clean = df.book
+
+#Changing variable names to something more simple
+df.book.clean$title=df.book.clean$my.link.title
+df.book.clean$book.author=df.book.clean$my.link.authorname
+df.book.clean$book.rating=df.book.clean$my.link.ratingtext
+
+#Selecting only relevant variables
+df.book.clean=select(df.book.clean, title, book.author, book.rating)
+
+#Splitting rating variable in two - First part gonna be used for rating, second part for numberofratings  
+splitstring=str_split(df.book.clean$book.rating,"—",n=Inf)
+head(splitstring)
+unique(splitstring)
+
+#unlisting splitstring to dataframe
+df <- data.frame(matrix(unlist(splitstring), nrow=1443, byrow=T))
+
+#Converting X1 and X2 to characters:
+df$X1 <- as.character(df$X1)
+df$X2 <- as.character(df$X2)
+
+#stringmanipulations on the two new variables:
+df$X1 = gsub("(\\d\\.\\d+\\s\\w+\\s\\d\\s\\w+\\s)","", df$X1) #Cleaning X1 by Regex
+df.book.clean$book.rating=gsub(" avg rating","",df$X1) #Inserting in the df.book.clean data frame
+
+df$X2= gsub(",","", df$X2) #Cleaning X2 by removing commas
+df.book.clean$books.numberofratings=gsub(" ratings","",df$X2) #Inserting in the df.book.clean data frame
+
+#---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------
+
+# Getting rid of duplicates (However: The Hunger Games (The Hunger Games, #1) appear twice due to one ekstra rating in one observation)
+df.book.unique=unique(df.book.clean)
+df.imdb.unique=unique(df.imdb.clean)
+
+
+# Merging the two data frames by an inner join on title.
+df.merged=join(df.book.unique, df.imdb.unique,
+     type = "inner")
+
+
+
 
 
 
